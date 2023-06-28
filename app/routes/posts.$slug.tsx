1333374ef3d4f88075c74a1fadd92b5c3766f0cc
lusan <daslusan@gmail.com>
2023-06-28 23:12:13 +0530

    feat: rendered markdown and added types marked

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,7 +1,9 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+
 import invariant from "tiny-invariant";
+import { marked } from "marked";
 
 import { getPost } from "~/models/post.server";
 
@@ -10,14 +12,16 @@ export const loader = async ({ params }: LoaderArgs) => {
   const post = await getPost(params.slug);
   invariant(post, `Post not found: ${params.slug}`);
 
-  return json({ post });
+  const html = marked(post.markdown);
+  return json({ html, post });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData<typeof loader>();
+  const { html, post } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
